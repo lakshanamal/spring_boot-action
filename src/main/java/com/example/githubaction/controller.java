d2ad3a970c555838dbdc28f927df22b6649dc1cf
LakshanAmal <lakshanamal100@gmail.com>
2021-10-28 01:40:25 +0530

    new endpoint added

---
+++ src/main/java/com/example/githubaction/controller.java
@@ -0,0 +1,12 @@
+package com.example.githubaction;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+    @GetMapping("/admin")
+    public String admin() {
+        return "Hello World from admin";
+    }
+}
