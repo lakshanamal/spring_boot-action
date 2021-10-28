15e0d036f65b2d28ebdb8323c718639ab193bd12
LakshanAmal <lakshanamal100@gmail.com>
2021-10-28 08:52:23 +0530

    employee endpoint added

--- src/main/java/com/example/githubaction/controller.java
+++ src/main/java/com/example/githubaction/controller.java
@@ -9,4 +9,9 @@ public class controller {
     public String admin() {
         return "Hello World from admin";
     }
+
+    @GetMapping("/employee")
+    public String employee() {
+        return "Hello World from admin";
+    }
 }
